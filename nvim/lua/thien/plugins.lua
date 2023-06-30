c105ecb5e75ca49b44a5fddde5c7d4cebb0211a8
Thien Nguyen <tatthien.contact@gmail.com>
2023-06-30 13:24:45 +0700

    nvim: add plugin markdown preview

--- nvim/lua/thien/plugins.lua
+++ nvim/lua/thien/plugins.lua
@@ -50,5 +50,13 @@ require('lazy').setup({
   { 'vim-scripts/sessionman.vim' },
   { 'tpope/vim-commentary' },
   { 'tpope/vim-fugitive' },
-  { 'vimwiki/vimwiki' }
+  { 'vimwiki/vimwiki' },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+  },
 })
