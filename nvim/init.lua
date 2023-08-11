84df56836ac25be47c81a93a62a7e9be47f258bb
Thien Nguyen <tatthien.contact@gmail.com>
2023-08-11 08:50:31 +0700

    nvim: key mapping for lspsaga

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,13 +93,14 @@ require("lazy").setup({
 			local diagnostic = require("lspsaga.diagnostic")
 			local opts = { noremap = true, silent = true }
 			vim.keymap.set("n", "<C-j>", "<Cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-			vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_diagnostic<CR>", opts)
-			vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
-			vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
-			-- vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 			vim.keymap.set("i", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+			vim.keymap.set("n", "K", "<Cmd>Lspsaga hover_doc<CR>", opts)
 			vim.keymap.set("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", opts)
 			vim.keymap.set("n", "gr", "<Cmd>Lspsaga rename<CR>", opts)
+			vim.keymap.set("n", "go", "<Cmd>Lspsaga outline<CR>", opts)
+			vim.keymap.set("n", "gf", "<Cmd>Lspsaga finder<CR>", opts)
+			vim.keymap.set("n", "gd", "<Cmd>Lspsaga lsp_finder<CR>", opts)
+			vim.keymap.set("n", "gl", "<Cmd>Lspsaga show_diagnostic<CR>", opts)
 
 			-- code action
 			local codeaction = require("lspsaga.codeaction")
