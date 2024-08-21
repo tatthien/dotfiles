a3578d31ba0a8b7d8f4bf2919ad2ad9b3b27fc11
Thien Nguyen <tatthien.contact@gmail.com>
2024-08-21 13:33:59 +0700

    chore: remove unknown Mason's config

--- nvim/init.lua
+++ nvim/init.lua
@@ -108,7 +108,6 @@ require("lazy").setup({
 		"williamboman/mason.nvim",
 		cmd = "Mason",
 		build = ":MasonUpdate",
-		opts_extend = { "ensure_installed" },
 		opts = {
 			ensure_installed = {
 				"stylua",
