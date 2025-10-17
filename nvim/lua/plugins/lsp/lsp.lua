84dfd4414404d5115ab0aa6dbac33e693d73eea8
Thien Nguyen <tatthien.contact@gmail.com>
2025-10-17 12:50:39 +0700

    nvim: update lsp config to compatible with neovim 0.11+

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -10,11 +10,11 @@ return {
       capabilities.textDocument.completion.completionItem.snippetSupport = true
 
       -- Change the Diagnostics signs
-      -- local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-      -- for type, icon in pairs(signs) do
-      --   local hl = "DiagnosticSign" .. type
-      --   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      -- end
+      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+      end
 
       local on_attach = function(_, bufnr)
         local function buf_set_option(...)
@@ -25,32 +25,174 @@ return {
         buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
       end
 
-      -- Enable LSP
-      local servers = {
-        "ts_ls",
-        "gopls",
-        "intelephense",
-        "terraformls",
-        "tflint",
-        "vimls",
-        "tailwindcss",
-        "lua_ls",
-        "cssls",
-        "cssmodules_ls",
-        "pyright",
-      }
+      -- TypeScript/JavaScript
+      vim.lsp.config("ts_ls", {
+        cmd = { "typescript-language-server", "--stdio" },
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+        },
+        root_markers = { ".git" },
+        capabilities = capabilities,
+        single_file_support = false,
+        init_options = {
+          preferences = {
+            disableSuggestions = false,
+          },
+        },
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "literal",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = false,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+      })
 
-      local lspconfig = require("lspconfig")
-      for _, lsp in ipairs(servers) do
-        lspconfig[lsp].setup({
-          on_attach = on_attach,
-          capabilities = capabilities,
-        })
-      end
+      -- Go
+      vim.lsp.config("gopls", {
+        cmd = { "gopls" },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_markers = { "go.work", "go.mod", ".git" },
+        capabilities = capabilities,
+      })
+
+      -- PHP
+      vim.lsp.config("intelephense", {
+        cmd = { "intelephense", "--stdio" },
+        filetypes = { "php" },
+        root_markers = { "composer.json", ".git" },
+        capabilities = capabilities,
+      })
+
+      -- Terraform
+      vim.lsp.config("terraformls", {
+        cmd = { "terraform-ls", "serve" },
+        filetypes = { "terraform", "terraform-vars" },
+        root_markers = { ".terraform", ".git" },
+        capabilities = capabilities,
+      })
+
+      -- TFLint
+      vim.lsp.config("tflint", {
+        cmd = { "tflint", "--langserver" },
+        filetypes = { "terraform" },
+        root_markers = { ".terraform", ".git", ".tflint.hcl" },
+        capabilities = capabilities,
+      })
+
+      -- Vim
+      vim.lsp.config("vimls", {
+        cmd = { "vim-language-server", "--stdio" },
+        filetypes = { "vim" },
+        root_markers = { ".git" },
+        capabilities = capabilities,
+      })
+
+      -- Tailwind CSS
+      vim.lsp.config("tailwindcss", {
+        cmd = { "tailwindcss-language-server", "--stdio" },
+        filetypes = {
+          "aspnetcorerazor",
+          "astro",
+          "astro-markdown",
+          "blade",
+          "clojure",
+          "django-html",
+          "htmldjango",
+          "edge",
+          "eelixir",
+          "elixir",
+          "ejs",
+          "erb",
+          "eruby",
+          "gohtml",
+          "gohtmltmpl",
+          "haml",
+          "handlebars",
+          "hbs",
+          "html",
+          "html-eex",
+          "heex",
+          "jade",
+          "leaf",
+          "liquid",
+          "markdown",
+          "mdx",
+          "mustache",
+          "njk",
+          "nunjucks",
+          "php",
+          "razor",
+          "slim",
+          "twig",
+          "css",
+          "less",
+          "postcss",
+          "sass",
+          "scss",
+          "stylus",
+          "sugarss",
+          "javascript",
+          "javascriptreact",
+          "reason",
+          "rescript",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "svelte",
+        },
+        root_markers = {
+          "tailwind.config.js",
+          "tailwind.config.cjs",
+          "tailwind.config.mjs",
+          "tailwind.config.ts",
+          "postcss.config.js",
+          "postcss.config.cjs",
+          "postcss.config.mjs",
+          "postcss.config.ts",
+          "package.json",
+          "node_modules",
+          ".git",
+        },
+        capabilities = capabilities,
+      })
 
       -- Lua
-      lspconfig.lua_ls.setup({
-        on_attach = on_attach,
+      vim.lsp.config("lua_ls", {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = {
+          ".luarc.json",
+          ".luarc.jsonc",
+          ".luacheckrc",
+          ".stylua.toml",
+          "stylua.toml",
+          "selene.toml",
+          "selene.yml",
+          ".git",
+        },
         capabilities = capabilities,
         single_file_support = true,
         settings = {
@@ -116,59 +258,37 @@ return {
         },
       })
 
-      -- TypeScript
-      lspconfig.ts_ls.setup({
-        on_attach = on_attach,
+      -- CSS
+      vim.lsp.config("cssls", {
+        cmd = { "vscode-css-language-server", "--stdio" },
+        filetypes = { "css", "scss", "less" },
+        root_markers = { "package.json", ".git" },
         capabilities = capabilities,
-        root_dir = function(...)
-          return lspconfig.util.root_pattern(".git")(...)
-        end,
-        init_options = {
-          preferences = {
-            disableSuggestions = false,
-          },
-        },
-        single_file_support = false,
-        settings = {
-          typescript = {
-            inlayHints = {
-              includeInlayParameterNameHints = "literal",
-              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = false,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-          },
-          javascript = {
-            inlayHints = {
-              includeInlayParameterNameHints = "all",
-              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-            },
-          },
-        },
+      })
+
+      -- CSS Modules
+      vim.lsp.config("cssmodules_ls", {
+        cmd = { "cssmodules-language-server" },
+        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+        root_markers = { "package.json" },
+        capabilities = capabilities,
       })
 
       -- Python
-      local python_root_files = {
-        "WORKSPACE", -- added for Bazel; items below are from default config
-        "pyproject.toml",
-        "setup.py",
-        "setup.cfg",
-        "requirements.txt",
-        "Pipfile",
-        "pyrightconfig.json",
-      }
-      lspconfig.pyright.setup({
-        on_attach = on_attach,
+      vim.lsp.config("pyright", {
+        cmd = { "pyright-langserver", "--stdio" },
+        filetypes = { "python" },
+        root_markers = {
+          "WORKSPACE",
+          "pyproject.toml",
+          "setup.py",
+          "setup.cfg",
+          "requirements.txt",
+          "Pipfile",
+          "pyrightconfig.json",
+          ".git",
+        },
         capabilities = capabilities,
-        root_dir = lspconfig.util.root_pattern(unpack(python_root_files)),
         settings = {
           python = {
             analysis = {
@@ -178,6 +298,32 @@ return {
             },
           },
         },
+      })
+
+      -- Enable all LSP servers
+      local servers = {
+        "ts_ls",
+        "gopls",
+        "intelephense",
+        "terraformls",
+        "tflint",
+        "vimls",
+        "tailwindcss",
+        "lua_ls",
+        "cssls",
+        "cssmodules_ls",
+        "pyright",
+      }
+
+      for _, lsp in ipairs(servers) do
+        vim.lsp.enable(lsp)
+      end
+
+      -- Setup on_attach for all buffers with LSP
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          on_attach(nil, args.buf)
+        end,
       })
     end,
   },
