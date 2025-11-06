7bfa92e278781c5433cae071e7ae1b2fe7ce5425
Thien Nguyen <tatthien.contact@gmail.com>
2025-11-06 19:59:56 +0700

    chore(nvim): enable dim_inactive

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,8 @@
-local palette = require("palette")
-
-local c = {
-  error = palette.red_light,
-  warning = palette.yellow_light,
-  info = palette.green_light,
-  hint = palette.aqua_light,
-}
-local bg = palette.bg
-local function blend_colors(color1, color2, alpha)
-  local r1, g1, b1 = tonumber(color1:sub(2, 3), 16), tonumber(color1:sub(4, 5), 16), tonumber(color1:sub(6, 7), 16)
-  local r2, g2, b2 = tonumber(color2:sub(2, 3), 16), tonumber(color2:sub(4, 5), 16), tonumber(color2:sub(6, 7), 16)
-  local r = math.floor(r1 * alpha + r2 * (1 - alpha))
-  local g = math.floor(g1 * alpha + g2 * (1 - alpha))
-  local b = math.floor(b1 * alpha + b2 * (1 - alpha))
-  return string.format("#%02X%02X%02X", r, g, b)
-end
-
 return {
   {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
     config = function()
-      -- Default options:
       require("gruvbox").setup({
         terminal_colors = true, -- add neovim terminal colors
         undercurl = true,
@@ -42,14 +23,14 @@ return {
         inverse = true, -- invert background for search, diffs, statuslines and errors
         contrast = "soft", -- can be "hard", "soft" or empty string
         palette_overrides = {},
-        dim_inactive = false,
+        dim_inactive = true,
         transparent_mode = false,
-        overrides = {
-          DiagnosticVirtualTextError = { bg = blend_colors(c.error, bg, 0.1), fg = c.error },
-          DiagnosticVirtualTextWarn = { bg = blend_colors(c.warning, bg, 0.1), fg = c.warning },
-          DiagnosticVirtualTextInfo = { bg = blend_colors(c.info, bg, 0.1), fg = c.info },
-          DiagnosticVirtualTextHint = { bg = blend_colors(c.hint, bg, 0.1), fg = c.hint },
-        },
+        -- overrides = {
+        --   DiagnosticVirtualTextError = { bg = blend_colors(c.error, bg, 0.1), fg = c.error },
+        --   DiagnosticVirtualTextWarn = { bg = blend_colors(c.warning, bg, 0.1), fg = c.warning },
+        --   DiagnosticVirtualTextInfo = { bg = blend_colors(c.info, bg, 0.1), fg = c.info },
+        --   DiagnosticVirtualTextHint = { bg = blend_colors(c.hint, bg, 0.1), fg = c.hint },
+        -- },
       })
       vim.o.background = "dark" -- or "light" for light mode
       vim.cmd([[colorscheme gruvbox]])
