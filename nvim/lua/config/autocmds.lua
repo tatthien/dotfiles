f803810d877a4cead9403ea320d67fccbc1df45c
Thien Nguyen <tatthien.contact@gmail.com>
2025-11-18 15:27:30 +0700

    feat(nvim): add user command to reveal the current file in Finder

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -34,3 +34,9 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "lua" },
   command = "set modifiable",
 })
+
+-- Reveal the current file in Finder
+vim.api.nvim_create_user_command("Rfinder", function()
+  local path = vim.api.nvim_buf_get_name(0)
+  os.execute("open -R " .. path)
+end, {})
