edae2ae485e1e38fc6fbfe9835a6e86190f3e58c
Thien Nguyen <tatthien.contact@gmail.com>
2026-01-14 21:42:41 +0700

    chore(nvim): reinstall plugin markdown-preview

--- nvim/lua/plugins/editor/markdown.lua
+++ nvim/lua/plugins/editor/markdown.lua
@@ -2,10 +2,9 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
-    end,
     ft = { "markdown" },
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
   },
 }
