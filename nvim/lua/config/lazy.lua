f7c8b35f59617ee34fc954258ad819410c279ca3
Daniel Nguyen <thien.nguyen@persuit.com>
2026-03-10 15:59:41 +0700

    feat(nvim): add extras.lang.typescript

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "plugins" },
     { import = "plugins.overrides" },
