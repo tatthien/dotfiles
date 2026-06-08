abccd39e3e3090e2f30f16480f4ab7e6ca1d1591
Daniel Nguyen <thien.nguyen@persuit.com>
2026-06-08 11:03:20 +0700

    feat: config biome for conform plugin

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,6 +4,9 @@
 
 vim.g.mapleader = "\\"
 
+-- Format on save (handled by conform.nvim). Toggle per-buffer/global with `<leader>uf`.
+vim.g.autoformat = true
+
 vim.opt.list = true
 vim.opt.listchars = {
   -- eol = "↓",
@@ -16,3 +19,12 @@ vim.opt.listchars = {
 
 -- Enable tsgo for Typescript LSP
 vim.g.lazyvim_ts_lsp = "tsgo"
+
+-- Suppress eslint -32603 "Cannot read config" spam when a project has no eslint config
+local orig_notify = vim.notify
+vim.notify = function(msg, level, opts)
+  if type(msg) == "string" and msg:find("eslint.*%-32603") then
+    return
+  end
+  orig_notify(msg, level, opts)
+end
