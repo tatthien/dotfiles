6ac7961fe0820e4a3f73438552e609b5d7feef7c
Daniel Nguyen <thien.nguyen@persuit.com>
2026-06-30 12:33:24 +0700

    feat: add keymap to Codediff

--- nvim/lua/plugins/editor/codediff.lua
+++ nvim/lua/plugins/editor/codediff.lua
@@ -2,5 +2,12 @@ return {
   {
     "esmuellert/codediff.nvim",
     cmd = "CodeDiff",
+    keys = {
+      {
+        "<leader>cd",
+        "<cmd>CodeDiff<cr>",
+        desc = "Open CodeDiff",
+      },
+    },
   },
 }
